1b9a7ea46884b20ffadb44cfd73be47dbaab85f7
Mykhailo Hariachyi <mykhailo.hariachyi@ext.addisongloballtd.com>
2019-12-05 17:18:14 +0100

    lotto test

--- src/test/java/rest/assured/demo/SimpleTest.java
+++ src/test/java/rest/assured/demo/SimpleTest.java
@@ -1,19 +1,52 @@
 package rest.assured.demo;
 
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.response.Response;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.JsonConfig.jsonConfig;
+import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 
 public class SimpleTest extends BaseTest {
 
+    private Response getLotto() {
+        return given()
+            .spec(myJsonServer)
+            .get("/lotto");
+    }
+
+    private Response getProducts() {
+        return given()
+            .config(RestAssuredConfig
+                .config()
+                .jsonConfig(jsonConfig()
+                    .numberReturnType(BIG_DECIMAL)))
+            .spec(myJsonServer)
+            .get("/products");
+    }
+
+
     @Test
     public void checkLottoId() {
-        given()
-            .spec(myJsonServer)
-            .get("/lotto")
+        getLotto()
             .then()
             .body("lotto.lottoId", equalTo(5));
+    }
+
+    @Test
+    public void checkWinnerId() {
+        getLotto()
+            .then()
+            .body("lotto.winners.winnerId", hasItems(23, 54));
+    }
+
+    @Test
+    public void checkPriceBigDecimal() {
+        getProducts()
+            .then()
+            .body(" find { it.@id == 2 }", hasItems("price"));
     }
 }
